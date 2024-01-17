ddb56eddd70067ba6657af6871af62c500c17ac8
Sachin Raut <117761849+YourBfSachin@users.noreply.github.com>
2024-01-17 22:28:31 +0545

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,17 @@
+// Sum of three numbers
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   
+ int a,b,c,d;
+    printf("Enter first no=");
+    scanf("%d",&a);
+    printf("Enter second no=");
+    scanf("%d",&b);
+    printf("Enter third no=");
+    scanf("%d",&c);
+    d=a+b+c;
+    printf("Sum=%d",d);
+    getch();
+}
