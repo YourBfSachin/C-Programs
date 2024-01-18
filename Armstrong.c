5cb54c5c02f0c353073ff6989b02ee7c1a87f82b
Sachin Raut <117761849+YourBfSachin@users.noreply.github.com>
2024-01-18 07:27:04 +0545

    Update Armstrong.c

--- Armstrong.c
+++ Armstrong.c
@@ -1,23 +1,24 @@
 #include<stdio.h>
-#include<conio.h>
-void main()
+
+int main()
 {
     int n,r,temp,sum=0;
     printf("Enter number=");
     scanf("%d",&n);
     temp=n;
+    if (n == 0) sum = 0; // handle the case when n is zero
     while(n>0)
     {
         r=n%10;
-        r*r*r;
+        // r*r*r; // this line has no effect, you can remove it or assign the result to r
         sum=sum+r*r*r;
         n=n/10;
     }
     n=temp;
     if (n==sum)
-    printf{"Armstrong"};
+    printf("Armstrong\n"); // use parentheses instead of curly braces, and add a newline character
     else 
-    printf("Not an Armstrong");
-    getch();
+    printf("Not an Armstrong\n"); // add a newline character
+    return 0;
 
 }
