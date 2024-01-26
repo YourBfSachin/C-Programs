22171e3b963acf666c6d2e911b05265c86ac1637
Sachin Raut <117761849+YourBfSachin@users.noreply.github.com>
2024-01-26 17:22:36 +0545

    Add files via upload

---
+++ nosorting.c
@@ -0,0 +1,30 @@
+// Not working as should be.
+#include<stdio.h>
+int main()
+{
+    int num[10],i,j,temp;
+    for(i=0;i<10;i++)
+    {
+        printf("Enter number %d=",i+1);
+        scanf("%d",&num[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<9;j++)
+        {
+            if(num[j]>num[j+1])
+            {
+                temp=num[j];
+                num[j]=num[j+1];
+                num[j+1]=temp;
+            }
+        }
+    }
+    printf("The numbers in ascending order are \n");
+    for(i=0;i<10;i++)
+    printf("%d\t",num[j]);
+    printf("The numbers in descending order are \n");
+    for(i=9;i>=0;i--)
+    printf("%d",num[i]);
+    return 0;
+}
