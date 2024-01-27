096cefb613440d6f4cd978673ab0ccf63597ea73
Sachin Raut <117761849+YourBfSachin@users.noreply.github.com>
2024-01-27 12:48:20 +0545

    Update Palindrome1.c

--- Palindrome1.c
+++ Palindrome1.c
@@ -1,20 +1,24 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-{
-int n,rev=0,r,c;
-printf("Enter a number=");
-scanf("%d",&n);
-c=n;
-while(n>0)
-{
-r=n%10;
-rev=rev*10+r;
-n=n/10;
-}
-if(rev==c)
-printf("The number is pallindrome");
-else
-printf("The number is not a pallindrome");
-getch();
+#include <stdio.h>
+
+int main() {
+    int n, rev = 0, r, c;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    c = n;  // Store the original number
+
+    while (n > 0) {
+        r = n % 10;
+        rev = rev * 10 + r;
+        n /= 10;  // Use integer division to avoid potential overflow
+    }
+
+    if (rev == c) {
+        printf("The number is a palindrome.\n");
+    } else {
+        printf("The number is not a palindrome.\n");
+    }
+
+    return 0;  // Indicate successful execution
 }
