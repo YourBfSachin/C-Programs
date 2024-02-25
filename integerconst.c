e0a7770ffbdac1e1d104291c3262176d5a71568e
Sachin Raut <117761849+YourBfSachin@users.noreply.github.com>
2024-02-25 13:04:20 +0545

    Add files via upload

---
+++ integerconst.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+a=10;  //10 is decimal
+b=010; //010 is an octal
+c=0xAA; //0xAA is hexadecimal
+printf("Decimal integer is %d",a);
+printf("\nOctal integer equivalent in decimal is %d",b);
+printf("\nHexadecimal integer equivalent in decimal is %d",c);
+return 0;
+}
