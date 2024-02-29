2e9ef2a6b357939cc672be09ccf15bd5aff5af64
Sachin Raut <117761849+YourBfSachin@users.noreply.github.com>
2024-02-29 23:57:55 +0545

    Update 2Darrayaddition.c

--- 2Darrayaddition.c
+++ 2Darrayaddition.c
@@ -1,7 +1,8 @@
+// Program to input size of matrix by user and add the two matrix and print it.
 #include<stdio.h>
 int main()
 {
-    int i,j,w,x,y,z;
+    int i,j,w,x,y,z,sum[i][j];;
     printf("Enter the number of rows and column of first array:");
     scanf("%d %d",&w,&x);
     printf("Enter the number of rows and column of second array:");
@@ -11,17 +12,26 @@ int main()
     {
         for(i=0;i<w;i++)
         {
-            for(j<0;j<x;j++)
+            for(j=0;j<x;j++)
             {
                 printf("Enter the value of Array[%d][%d]:\t",i,j);
                 scanf("%d",&A1[i][j]);
             }
             printf("\n");
+        }        
+        for(i=0;i<y;i++)
+        {
+            for(j=0;j<z;j++)
+            {
+                printf("Enter the value of Array[%d][%d]: \t",i,j);
+                scanf("%d",&A2[i][j]);
+            }
+            printf("\n");
         }
-        int sum[i][j];
+        
         for(i=0;i<w;i++)
         {
-            for(j<0;j<x;j++)
+            for(j=0;j<x;j++)
             {
                 sum[i][j]=A1[i][j]+A2[i][j];
             }
@@ -29,7 +39,7 @@ int main()
         }
         for(i=0;i<w;i++)
         {
-            for(j<0;j<x;j++)
+            for(j=0;j<x;j++)
             {
                 printf("%d\t",sum[i][j]);                
             }
